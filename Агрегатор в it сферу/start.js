57578350ae2e0a99bb3d54e9eea5f9f2bf1fbdb3
Computer-Dataa <cactus200726@gmail.com>
2025-01-27 04:29:20 +0500

    finaly

---
+++ Агрегатор в it сферу/start.js
@@ -0,0 +1,10 @@
+// const selector = document.querySelector('.founadtion');
+// selector.classList.add('magictime', 'vanishOut');
+
+// function animate() {
+//     const animka = document.querySelector('.foundation')
+//     selector.classList.add('magictime', 'vanishOut')
+// }
+
+//setInterval(animate, 3000);
+
